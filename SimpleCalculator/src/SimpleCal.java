902dd2eaef7fe927a09feffb5d7eeec69b674a25
vic <vic@192-168-1-8.tpgi.com.au>
2016-03-07 10:12:49 +1100

    v1

--- SimpleCalculator/src/SimpleCal.java
+++ SimpleCalculator/src/SimpleCal.java
@@ -3,10 +3,28 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JToolBar;
 import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Window.Type;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.JSeparator;
+import java.awt.Dimension;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.ActionEvent;
+import javax.swing.JTabbedPane;
+import java.awt.Cursor;
 
 public class SimpleCal {
 
-	private JFrame frame;
+	private JFrame frmCalculator;
 
 	/**
 	 * Launch the application.
@@ -16,7 +34,7 @@ public class SimpleCal {
 			public void run() {
 				try {
 					SimpleCal window = new SimpleCal();
-					window.frame.setVisible(true);
+					window.frmCalculator.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,12 +53,101 @@ public class SimpleCal {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCalculator = new JFrame();
+		frmCalculator.setTitle("Calculator");
 		
-		JToolBar toolBar = new JToolBar();
-		frame.getContentPane().add(toolBar, BorderLayout.NORTH);
+		JButton no_1 = new JButton("1");
+		no_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+							}
+		});
+		
+		JButton no_2 = new JButton("2");
+		no_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton no_3 = new JButton("3");
+		
+		JButton no_4 = new JButton("4");
+		
+		JButton no_5 = new JButton("5");
+		
+		JButton no_7 = new JButton("6");
+		
+		JButton button_3 = new JButton("7");
+		
+		JButton no_8 = new JButton("8");
+		
+		JButton no_9 = new JButton("9");
+		
+		JButton no_0 = new JButton("0");
+		no_0.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+		
+		JButton period = new JButton(".");
+		period.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GroupLayout groupLayout = new GroupLayout(frmCalculator.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(192, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(no_1)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(no_2, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+							.addGap(3)
+							.addComponent(no_3, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(no_4, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+							.addGap(3)
+							.addComponent(no_5, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(no_7, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(no_0, GroupLayout.PREFERRED_SIZE, 156, GroupLayout.PREFERRED_SIZE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(button_3, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(no_8, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(no_9, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+								.addComponent(period, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE))))
+					.addGap(21))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap(117, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(no_2)
+						.addComponent(no_3)
+						.addComponent(no_1))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(no_4)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+							.addComponent(no_7)
+							.addComponent(no_5)))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(button_3)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+							.addComponent(no_9)
+							.addComponent(no_8)))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(no_0)
+						.addComponent(period))
+					.addGap(27))
+		);
+		frmCalculator.getContentPane().setLayout(groupLayout);
+		frmCalculator.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{frmCalculator.getContentPane()}));
 	}
-
 }
